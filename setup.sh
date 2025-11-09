1d46761148d44730e3351125fac077eee723e5f3
Bastion-Scar <vanavanavic782@gmail.com>
2025-11-09 15:49:27 +0500

    build: add OS/arch detection and macOS support to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 set -e
 
+# Detect OS and architecture
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+case $ARCH in
+    x86_64) ARCH="amd64" ;;
+    aarch64) ARCH="arm64" ;;
+    arm64) ARCH="arm64" ;;
+    *) ARCH="amd64" ;;
+esac
+
+echo "Detected OS: $OS, Architecture: $ARCH"
+
 # Building docker image
+echo "Building docker image..."
 docker build -t sco-builder .
 
 # Creating temporary container
-docker create --name temp sco-builder
-sudo docker cp temp:/usr/local/bin/sco /usr/local/bin/sco #Copy binary from container to our machine
-docker rm temp
+echo "Creating container..."
+container_id=$(docker create sco-builder)
 
-# Making file executable
-sudo chmod +x /usr/local/bin/sco
+# Determine binary name and install path
+if [ "$OS" = "linux" ]; then
+    BINARY_NAME="sco-linux"
+    INSTALL_PATH="/usr/local/bin/sco"
+    COPY_CMD="sudo docker cp"
+    CHMOD_CMD="sudo chmod"
+elif [ "$OS" = "darwin" ]; then
+    BINARY_NAME="sco-macos" 
+    INSTALL_PATH="/usr/local/bin/sco"
+    COPY_CMD="docker cp"
+    CHMOD_CMD="chmod"
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
 
-echo "scarycommit installed в /usr/local/bin"
+# Copy appropriate binary
+echo "Copying $BINARY_NAME to $INSTALL_PATH..."
+$COPY_CMD "$container_id:/usr/local/bin/$BINARY_NAME" "$INSTALL_PATH"
+
+# Remove container
+docker rm "$container_id"
+
+# Make executable
+$CHMOD_CMD +x "$INSTALL_PATH"
+
+echo "✅ scarycommit installed to $INSTALL_PATH"
+echo "Done! You can now run: sco"
  # IIIITSSS TTVV TIIIIMEEE
